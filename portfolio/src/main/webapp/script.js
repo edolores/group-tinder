3e2403bf59c683d2de200a3b64af6c04c3b208c0
ErickDolores <edolores@sps-program.com>
2020-04-18 00:31:29 -0700

    finish rough frontend of profile names page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+var namesCheckedOff;
 
 function getChat() {
     fetch('/data')
@@ -23,14 +24,81 @@ function getChat() {
         myStrings.innerHTML = '';
         funFacts.comments.forEach((line) => {
             console.log(line);
-            myStrings.appendChild(createListElement(line));
+            // check list element inner text
+            var liElement = createListElement(line);
+            // var n = liElement.innerText.includes('test-user');
+            // console.log(n);
+            // if(n){
+            //     liElement.style = 'float:right;';
+            // } else{
+            //     liElement.style = 'float:left;';
+            // }
+            myStrings.appendChild(liElement);
         });
     });
 }
 
+function addTable() {
+    namesCheckedOff = 0;
+    const nameCount = document.getElementById('names-text-test');
+    nameCount.innerText = namesCheckedOff;
+    var tablearea = document.getElementById('names-table'),
+        table = document.createElement('table');
+    table.id = 'profile-list';
+    table.classList.add('list-table');
+
+    for (var i = 0; i < 20; i++) {
+        //create a new table row
+        var name = 'Andrew Boyle';
+        var tr = document.createElement('tr');
+        //creates td
+        var td = document.createElement('td');
+        //each td requires it's own checkbox variable
+        //td also has a name field that may be useful
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.style = 'width:40px;height:40px;';
+        checkbox.addEventListener('click', function() {
+            if(!this.checked){
+                this.checked = true;
+            } else{
+                this.checked = false;
+            }
+        });
+        //append the checkbox to the td
+        td.appendChild(checkbox);
+        //add event listentener for when user clicks td
+        td.addEventListener('click', function() {
+            var cbox = this.childNodes[0];
+            if(!cbox.checked){
+                cbox.checked = true;
+                namesCheckedOff++;
+            } else{
+                namesCheckedOff--;
+                cbox.checked = false;
+            }
+            nameCount.innerText = namesCheckedOff;
+        });
+
+        //append the newly created td's to the tr
+        tr.appendChild(td);
+        //add actual text to the cells of the table row
+        tr.cells[0].appendChild(document.createTextNode(name));
+        // if(i%2==0){
+        //     tr.childNodes[0].align = 'right';
+        //     // tr.childNodes[0].text-align = 'left';
+        // } else{
+        //     tr.align = 'left';
+        // } 
+        table.appendChild(tr);
+    }
+
+    tablearea.appendChild(table);
+}
+
 // TODO: Figure out if you need anything dynamic on this web page
-function getCategories() {
-    fetch('/data')
+function getProfiles() {
+    fetch('/profiles')
     .then(response => response.json())
     .then((funFacts) => {
         const uname  = document.getElementById('username');
