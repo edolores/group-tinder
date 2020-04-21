654ce8d2bff1fb9bf21b2550df3aaf2e6e8391b7
ErickDolores <edolores@sps-program.com>
2020-04-20 21:03:27 -0700

    Replicate the code for Video Games Category backend for the five other categories. Change header and title of video games chat.

---
+++ project/src/main/java/com/google/sps/servlets/FoodProfilesDataServlet.java
@@ -0,0 +1,80 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*; 
+import com.google.gson.Gson;
+
+/* Servlet that handles user submitting names for groupchat*/
+@WebServlet("/food-group-names")
+public class FoodProfilesDataServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("FoodProfiles");
+    
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    // Retrieve usernames that are in the groupchat
+    ArrayList<String> list = new ArrayList<String>();
+    for(Entity entity: results.asIterable()){
+        String uname = (String) entity.getProperty("Username");
+        list.add(uname);
+    }
+
+    response.setContentType("application/json");
+    Gson gson = new Gson();
+    String unames = gson.toJson(list);
+
+    // Create json for usernames
+    StringBuilder json = new StringBuilder();
+    json.append("{\"usernames\": ");
+    json.append(unames);
+    json.append("}");
+    
+    response.getWriter().println(json);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Using the string of names, create a list of the usernames
+    String nameInput = request.getParameter("profile-name-list");
+    List<String> profileNameList = new ArrayList<String>(Arrays.asList(nameInput.split(",")));
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+    // Add a new entity for each username
+    for(String pName: profileNameList){
+        Entity foodEntity = new Entity("FoodProfiles");
+        foodEntity.setProperty("Username", pName);
+        datastore.put(foodEntity);
+    }
+
+    // Redirect back to the chat page.
+    response.sendRedirect("/food-chat.html");
+  }
+}
---
+++ project/src/main/java/com/google/sps/servlets/MusicChatDataServlet.java
@@ -0,0 +1,114 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*; 
+import com.google.gson.Gson;
+
+/* Servlet that handles getting and posting messages to group chat*/
+@WebServlet("/music-data")
+public class MusicChatDataServlet extends HttpServlet {
+
+  private String username = "";
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("MusicChat").addSort("Timestamp", SortDirection.ASCENDING);
+    
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    // Build list of comment strings
+    ArrayList<String> list = new ArrayList<String>();
+    for(Entity entity: results.asIterable()){
+        StringBuilder str = new StringBuilder();
+        String com = (String) entity.getProperty("Username");
+        if(com == null){
+            str.append("");
+        }
+        else{
+            str.append(com);
+            str.append(": ");
+        }
+        com = (String) entity.getProperty("Content");
+        str.append(com);
+        list.add(str.toString());
+    }
+
+    response.setContentType("application/json");
+    Gson gson = new Gson();
+    String comments = gson.toJson(list);
+
+    /*Create json with username and comments*/
+    StringBuilder json = new StringBuilder();
+    json.append("{\"username\":\"");
+    json.append(username);
+    json.append("\", \"comments\": ");
+    json.append(comments);
+    json.append("}");
+    
+    response.getWriter().println(json);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    String nameInput = request.getParameter("name");
+
+    //if the username is different and isn't empty
+    if((nameInput != username) && (nameInput.length() != 0)){
+        username = nameInput;
+    }
+    else if((nameInput.length()) == 0 && (username.length() == 0)){
+      response.setContentType("text/html");
+      response.getWriter().println("Please enter a non-empty username");
+      return;
+    }
+
+    // Get the input from the form.
+    String input = request.getParameter("text-input");
+
+    //if input is empty ask them to enter again
+    if (input.length() == 0) {
+      response.setContentType("text/html");
+      response.getWriter().println("Please enter a valid string");
+      return;
+    }
+
+    long timestamp = System.currentTimeMillis();
+
+    Entity chatEntity = new Entity("MusicChat");
+    chatEntity.setProperty("Username", username);
+    chatEntity.setProperty("Content", input);
+    chatEntity.setProperty("Timestamp", timestamp);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(chatEntity);
+
+    // Redirect back to the HTML page.
+    response.sendRedirect("/music-chat.html");
+  }
+}
